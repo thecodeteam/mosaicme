a9d6b52abbc6f49b8d26f27367c289722270b748
Moreno, Adrian <adrian@morenomartinez.com>
2015-04-07 12:09:06 +0200

    added puppet manifest

---
+++ puppet/manifest/default.pp
@@ -0,0 +1,31 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+exec { 'apt-get update':
+  command => 'apt-get update',
+  timeout => 60,
+  tries   => 3
+}
+
+class { 'apt':
+  always_apt_update => true,
+}
+
+package { ['python-software-properties']:
+  ensure  => 'installed',
+  require => Exec['apt-get update'],
+}
+
+$sysPackages = ['git', 'curl']
+package { $sysPackages:
+  ensure => "installed",
+  require => Exec['apt-get update'],
+}
+
+class { 'python' :
+    version    => 'system',
+    pip        => true,
+    dev        => true,
+    gunicorn   => true,
+  }
+
+python::requirements { '/tmp/requirements.txt': }
