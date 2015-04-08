579facd7219856d64d6632313769a91315dadf72
Moreno, Adrian <adrian@morenomartinez.com>
2015-04-08 12:45:16 +0200

    twitter collector gets image urls

--- mosaicme/twitter-collector/twitter-collector.py
+++ mosaicme/twitter-collector/twitter-collector.py
@@ -4,10 +4,15 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import json
 import logging
+import logging.config
 import dotenv
 import os
 import sys
+
+logging.config.fileConfig('../logging.conf')
+logger = logging.getLogger('twitterCollector')
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 dotenv.read_dotenv(os.path.join(BASE_DIR, '..', '.env'))
@@ -18,21 +23,42 @@ try:
     access_token = os.environ['TWITTER_ACCESS_TOKEN']
     access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 except KeyError, e:
-    logging.error('Could not obtain environment variable: %s', str(e))
+    logger.error('Could not obtain environment variable: %s', str(e))
     sys.exit(1)
 
 
 class MosaicmeListener(StreamListener):
-    """ A listener handles tweets are the received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
-
     """
-    def on_data(self, data):
-        print(data)
+    This listener handles tweets that contain the #EMCWORLD hashtag and
+    gets the pictures
+    """
+    def on_data(self, str_data):
+        # print(str_data)
+
+        try:
+            data = json.loads(str_data)
+        except ValueError, e:
+            logger.warning('Could not parse JSON data. %s', str(e))
+            return True
+
+        if 'extended_entities' not in data:
+            logger.info('No extended entities found')
+            return True
+        if 'media' not in data['extended_entities']:
+            logger.info('No media found')
+        if not data['extended_entities']['media']:
+            logger.info('No media found')
+        for media in data['extended_entities']['media']:
+            if media['type'] != 'photo':
+                pass
+            media_url = media['media_url']
+            media_id = media['id_str']
+            logger.info('Media found. ID: %s - URL: %s', media_id, media_url)
         return True
 
     def on_error(self, status):
         print(status)
+
 
 if __name__ == '__main__':
     l = MosaicmeListener()
@@ -40,4 +66,4 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(track=['#MosaicMeTest1'])
+    stream.filter(track=['#felizmiercoles'])
