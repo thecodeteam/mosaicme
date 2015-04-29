08759e287670573763aade35ce357f20a1f38d74
Moreno, Adrian <adrian@morenomartinez.com>
2015-04-29 12:08:12 -0700

    added gallery and pagination to webapp

--- mosaicme/web/mosaic/static/js/app.js
+++ mosaicme/web/mosaic/static/js/app.js
@@ -1,4 +1,4 @@
-var mosaicmeApp = angular.module('mosaicmeApp', []).config(function ($httpProvider) {
+var mosaicmeApp = angular.module('mosaicmeApp', ['ui.bootstrap']).config(function ($httpProvider) {
 
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -14,23 +14,35 @@ mosaicmeApp
     })
     .controller('MosaicMeCtrl', function ($scope, $http, $log) {
 
+        $scope.$on('onRepeatLast', function (scope, element, attrs) {
+            $("#carousel1").tiksluscarousel(
+                {
+                    width: 0,
+                    height: 0,
+                    nav: 'thumbnails',
+                    current: 1,
+                    autoplayInterval: 5000,
+                    loader: 'static/images/ajax-loader.gif'
+                });
+            });
+
+        $scope.pageChanged = function() {
+            $log.log('Page changed to: ' + $scope.currentPage);
+
+            var start = ($scope.currentPage-1) * $scope.itemsPerPage;
+            $scope.displayImages = $scope.allImages.slice(start, start + $scope.itemsPerPage);
+
+        };
+
+        $scope.currentPage = 1;
+        $scope.itemsPerPage = 2;
+
         $http.get('/mosaic').
             success(function (data, status, headers, config) {
+                $scope.allImages = data['images'];
+                $scope.totalItems = data['size'];
 
-                $scope.$on('onRepeatLast', function (scope, element, attrs) {
-                    $("#carousel1").tiksluscarousel(
-                        {
-                            width: 0,
-                            height: 0,
-                            nav: 'thumbnails',
-                            current: 1,
-                            autoplayInterval: 5000,
-                            loader: 'static/images/ajax-loader.gif'
-                        });
-                });
-
-                $scope.images = data['images'];
-
+                $scope.pageChanged();
             }).
             error(function (data, status, headers, config) {
                 alert('Error getting images!');
