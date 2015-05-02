fb13391ba9459cf07ce592df95328d6b1fdcebba
Moreno, Adrian <adrian@morenomartinez.com>
2015-05-02 11:22:36 -0700

    fixed static collector and ignore static folder

--- mosaicme/web/web/settings.py
+++ mosaicme/web/web/settings.py
@@ -89,6 +89,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 S3_HOST = os.environ['S3_HOST']
