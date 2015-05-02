fc3b59571f58afe4e86768acacc095ee1457f712
Moreno, Adrian <adrian@morenomartinez.com>
2015-05-02 10:36:57 -0700

    added site logo. added twitter widget. disabled admin backend

--- mosaicme/web/web/urls.py
+++ mosaicme/web/web/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^admin/', include(admin.site.urls)),
                        url(r'^', include('mosaic.urls')),
 )
