22fb10e5d144c2aabffb0261dd2be0eb283c2e46
Adrian Moreno <adrian@morenomartinez.com>
2016-09-20 16:47:10 +0200

    Updates uploader task of engine service

--- mosaicme/engine/mosaicme/engine_uploader.go
+++ mosaicme/engine/mosaicme/engine_uploader.go
@@ -9,6 +9,9 @@ import (
   "github.com/streadway/amqp"
 )
 
+// uploader listens to a channel for messages from the builder goroutine with
+// mosaics, uploads them to the object storage backend, and notifies the
+// Publisher service once it's done
 func (e *Engine) uploader() {
   e.wg.Add(1)
   defer func() {
@@ -20,54 +23,62 @@ func (e *Engine) uploader() {
     select {
     case m := <-e.uploaderChan:
 
-      log.Println("2")
-      time.Sleep(10000 * time.Millisecond)
+      log.Printf("[Uploader] %s - Uploading high-res mosaic\n", m.MosaicName)
 
-      //Uploade Large file
       objectLarge, err := os.Open(m.MosaicPath)
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error opening high-res mosaic in local filesystem: %s\n", m.MosaicName, err)
+        continue
       }
-      defer objectLarge.Close()
 
       n, err := e.s3Client.PutObject(e.config.BucketOut, "large/"+m.MosaicName, objectLarge, "application/octet-stream")
+      objectLarge.Close()
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error uploading high-res mosaic to object store: %s\n", m.MosaicName, err)
+        continue
       }
-      log.Println("Uploaded large", m.MosaicName, " of size: ", n, "Successfully.")
-      //presign the large URL
+
+      log.Printf("[Uploader] %s - Uploaded high-res mosaic successfully. Size: %d\n", m.MosaicName, n)
+      log.Printf("[Uploader] %s - Generating presigned URL for high-res mosaic\n", m.MosaicName)
+
       presignedLargeURL, err := e.s3Client.PresignedPutObject(e.config.BucketOut, "large/"+m.MosaicName, time.Second*24*60*60*7)
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error signing URL for high-res mosaic: %s\n", m.MosaicName, err)
+        continue
       }
-      log.Println(presignedLargeURL)
 
-      //Uploade Small file
+      log.Printf("[Uploader] %s - Generated presigned URL for high-res mosaic: %s\n", m.MosaicName, presignedLargeURL)
+      log.Printf("[Uploader] %s - Uploading mosaic thumbnail\n", m.MosaicName)
+
       objectSmall, err := os.Open(m.ThumbnailPath)
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error opening thumbnail in local filesystem: %s\n", m.MosaicName, err)
+        continue
       }
-      defer objectLarge.Close()
 
       n, err = e.s3Client.PutObject(e.config.BucketOut, "small/"+m.MosaicName, objectSmall, "application/octet-stream")
+      objectSmall.Close()
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error uploading thumbnail to object store: %s\n", m.MosaicName, err)
+        continue
       }
-      log.Println("Uploaded thumnail", m.MosaicName, " of size: ", n, "Successfully.")
 
-      //presign the thunmail URL
+      log.Printf("[Uploader] %s - Uploaded mosaic thumbnail successfully. Size: %d\n", m.MosaicName, n)
+      log.Printf("[Uploader] %s - Generating presigned URL for mosaic thumbnail\n", m.MosaicName)
+
       presignedURL, err := e.s3Client.PresignedPutObject(e.config.BucketOut, "small/"+m.MosaicName, time.Second*24*60*60*7)
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error signing URL: %s\n", m.MosaicName, err)
+        continue
       }
 
-      log.Printf("Current presing URL: %s\n", presignedURL)
+      log.Printf("[Uploader] %s - Generated presigned URL for mosaic thumbnail: %s\n", m.MosaicName, presignedURL)
+      log.Printf("[Uploader] %s - Sending message to Publisher\n", m.MosaicName)
 
-      log.Printf("Send message to Twitter Publisher component ")
-
-      mJson, err := json.Marshal(*m)
+      mJSON, err := json.Marshal(*m)
       if err != nil {
-        log.Fatalln(err)
+        log.Printf("[Uploader] %s - Error parsing message: %s\n", m.MosaicName, err)
+        continue
       }
 
       err = e.channel.Publish(
@@ -77,16 +88,18 @@ func (e *Engine) uploader() {
         false,      // immediate
         amqp.Publishing{
           ContentType: "application/json",
-          Body:        mJson,
+          Body:        mJSON,
         })
+
       if err != nil {
-        log.Printf("Queue Consume Error: %s\n", err)
-        close(e.stopchan)
-        return
+        log.Printf("[Uploader] %s - Error sending message to Publisher: %s\n", m.MosaicName, err)
+        continue
       }
 
+      log.Printf("[Uploader] %s - Sent message to Twitter Publisher\n", m.MosaicName)
+
     case <-e.stopchan:
-      log.Println("Stop signal received. Returning...")
+      log.Println("[Uploader] Stop signal received. Returning...")
       return
     }
   }
